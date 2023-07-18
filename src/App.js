0bb0dff545a8dd592438b02b98eb829fa461cf33
Sam Steel <samuel.ini145@gmail.com>
2023-07-18 19:34:19 +0100

    Finished UI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
+import React from 'react';
 import './App.css';
 
-function App() {
+
+export default function App() {
   return (
-    <div className="App">
-      <h4>Sam</h4>
+    <div className='Container'>
+      <h1>Todo List</h1>
+      <hr />
+
+
+      <div className='newtask'>
+        <input type="text" placeholder="Add your task" />
+          <button className='submit'>Submit</button>    
       </div>
+<hr />
+
+      <ul>
+        <li className='lists'>
+          <span>Walk the dog</span>
+          <button>Delete</button>
+        </li>
+        <li>
+          <span>Water the plants</span>
+          <button>Delete</button>
+        </li>
+        <li>
+          <span>Wash the dishes</span>
+          <button>Delete</button>
+        </li>
+      </ul>
+    </div>
   );
 }
-
-export default App;
